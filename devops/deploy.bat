c77cdb08735d17f4d586d0ad55c842538fdb10a6
Clay Benoit <clay@mandarincreativegroup.com>
2017-12-01 18:57:02 -0500

    CL-50-2: Move deployment script to Web project

--- devops/deploy.bat
+++
@@ -1,7 +0,0 @@
-cd ../cryptokuma-marketing-io
-
-aws s3 cp --recursive wwwroot "s3://cryptokuma-marketing-web" --profile finexus
-
-aws cloudfront create-invalidation --distribution-id E3V14KBUG8N9E8 --paths /index.html /favicon.ico /assets/* --profile finexus
-
-cd ../devops
