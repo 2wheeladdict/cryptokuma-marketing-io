28fdb37c040367b9dcdbb47950986b42e4dbec87
Clay Benoit <clay@mandarincreativegroup.com>
2017-12-01 15:15:45 -0500

    Path fixes, stubbed favicon, deployment script invalidation updates

--- devops/deploy.bat
+++ devops/deploy.bat
@@ -2,6 +2,6 @@ cd ../cryptokuma-marketing-io
 
 aws s3 cp --recursive wwwroot "s3://cryptokuma-marketing-web" --profile finexus
 
-aws cloudfront create-invalidation --distribution-id E3V14KBUG8N9E8 --paths /index.html /assets/* --profile finexus
+aws cloudfront create-invalidation --distribution-id E3V14KBUG8N9E8 --paths /index.html /favicon.ico /assets/* --profile finexus
 
 cd ../devops
